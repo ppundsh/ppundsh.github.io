e55b689379da5a08b661afdda8bee0098de03317
ppundsh <v789987v@gmail.com>
2018-04-15 05:27:26 +0800

    Site updated: 2018-04-15 05:27:25

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,23 +1,3 @@
-$(document).ready(function() {
-
-  // Set relative link path (without domain)
-  var rpath = window.location.href.replace(window.location.origin, "");
-
-  // Write position in cookie
-  var timeout;
-  $(window).on("scroll", function() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      Cookies.set("scroll-cookie", ($(window).scrollTop() + "|" + rpath), { expires: 365, path: '' });
-    }, 250);
-  });
-
-  // Read position from cookie
-  if (Cookies.get("scroll-cookie") !== undefined) {
-    var cvalues = Cookies.get("scroll-cookie").split('|');
-      if (cvalues[1] == rpath) {
-        $(window).scrollTop(cvalues[0]);
-      }
-  }
-
-});
+// build time:Sun Apr 15 2018 05:27:14 GMT+0800 (台北標準時間)
+$(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
+//rebuild by neat 
