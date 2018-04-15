083b6ef1308e7ff437f861ea49c7d7d040facc5e
ppundsh <v789987v@gmail.com>
2018-04-15 14:40:09 +0800

    Site updated: 2018-04-15 14:40:07

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 15 2018 05:29:31 GMT+0800 (台北標準時間)
+// build time:Sun Apr 15 2018 14:39:57 GMT+0800 (台北標準時間)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
