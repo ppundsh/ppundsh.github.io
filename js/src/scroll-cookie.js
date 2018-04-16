d87eae140b65289c99ce4b0798669bf1a2d86a64
ppundsh <v789987v@gmail.com>
2018-04-16 09:45:43 +0800

    Site updated: 2018-04-16 09:45:41

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Apr 16 2018 09:23:37 GMT+0800 (台北標準時間)
+// build time:Mon Apr 16 2018 09:45:38 GMT+0800 (台北標準時間)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
