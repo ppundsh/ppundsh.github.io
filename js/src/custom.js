4f423297b32ead6d3b0b49f146d977d63ce96b7a
ppundsh <v789987v@gmail.com>
2018-04-27 15:10:05 +0800

    Site updated: 2018-04-27 15:10:01

--- js/src/custom.js
+++ js/src/custom.js
@@ -28,7 +28,7 @@ function createCopyBtns() {
         //值得注意的是：1.按鈕默認隱藏，2.位置使用絕對位置 position: absolute; (position: fixed 也可以，需要修改代碼)
         $(".post-body").before('<div id="copyBtn" style="opacity: 0; position: absolute;top:0px;left:0px;display: none;line-height: 1; font-size:1.5em"><span id="imgCopy" ><i class="fa fa-paste fa-fw"></i></span><span id="imgSuccess" style="display: none;"><i class="fa fa-check-circle fa-fw" aria-hidden="true"></i></span>');
         //創建 複製 插件，綁定單機時間到 指定元素，支援JQuery
-        var clipboard = new Clipboard('#copyBtn', {
+        var clipboard = new ClipboardJS('#copyBtn', {
             target: function() {
                 //返回需要複製的元素內容
                 return document.querySelector("[copyFlag]");
